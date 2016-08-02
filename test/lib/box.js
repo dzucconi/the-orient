3966a48699bc45ac34026974949333ced5d9c6ab
dzucconi <mail@damonzucconi.com>
2016-08-02 12:21:05 +0200

    Appease eslint

--- test/lib/box.js
+++ test/lib/box.js
@@ -1,6 +1,6 @@
 import box from '../../lib/box';
 
-describe('#box', () => {
+describe('box', () => {
   it('accepts a heading Float and returns the appropriate heading data (100.0)', () => {
     box(100.0).compass_point
       .should.equal('East by south');
@@ -13,6 +13,6 @@ describe('#box', () => {
 
   it('accepts a heading Float and returns the appropriate heading data (354.37)', () => {
     box(354.37).compass_point
-      .should.equal('North by west')
+      .should.equal('North by west');
   });
 });
